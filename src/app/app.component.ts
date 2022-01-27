9e892541ab626cb7995592501f32d1514cbf6170
Julio Berina <julioberina@gmail.com>
2022-01-27 00:15:15 -0800

    Setup toolbar and grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as randomWords from 'random-words';
+
+enum Letter {
+  Blank,
+  Wrong,
+  Misplaced,
+  Correct
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  public words: string[] = [];
+  public grid: string[][] = [
+    ['', '', '', '', ''], 
+    ['', '', '', '', ''], 
+    ['', '', '', '', ''], 
+    ['', '', '', '', ''], 
+    ['', '', '', '', ''], 
+    ['', '', '', '', '']
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.words = randomWords({ exactly: 5, maxLength: 5 });
+    console.log(JSON.stringify(this.grid));
+  }
 }
