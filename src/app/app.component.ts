1c9921fccd62bc7006d97053fbc1a270977052df
Julio Berina <julioberina@gmail.com>
2022-01-29 13:43:13 -0800

    Keyboard for UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,14 @@ enum Letter {
 })
 export class AppComponent implements OnInit {
   
+  public myRow = 0;
+  public myCol = 0;
+  public myWord = '';
+
+  public kbFirstRow = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+  public kbSecondRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+  public kbThirdRow = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
   public words: string[] = [];
   public grid: string[][] = [
     ['', '', '', '', ''], 
@@ -29,6 +37,19 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.words = randomWords({ exactly: 5, maxLength: 5 });
-    console.log(JSON.stringify(this.grid));
+  }
+
+  typeLetter(letter: string) {
+    if (this.myCol < 5) {
+      this.grid[this.myRow][this.myCol] = letter;
+      this.myCol += 1;
+    }
+  }
+
+  eraseLetter() {
+    if (this.myCol) {
+      this.grid[this.myRow][this.myCol - 1] = '';
+      this.myCol -= 1;
+    }
   }
 }
